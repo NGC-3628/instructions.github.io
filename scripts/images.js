c3c83fb4a788901fed2c8ba47f720183c57fdb19
NGC-3628 <isaacvgn@gmail.com>
2025-07-19 13:34:58 -0600

    modJSON

--- scripts/images.js
+++ scripts/images.js
@@ -1,1 +1,8 @@
-const url = "";
+const url = "https://github.com/NGC-3628/instructions.github.io/blob/main/images/imges.JSON";
+
+document.addEventListener("DOMContentLoaded", () => {
+    const images = document.querySelector('div.images');
+    getInstructionDetails().then(details => displayDetails(details));
+});
+
+
